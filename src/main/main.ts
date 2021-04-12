35a66b9a9b906cd2094cf5dc85d82e2042a05697
MichaelTugby <michael.tugby5@gmail.com>
2021-04-12 16:51:08 +0100

    Update dependencies

--- src/main/main.ts
+++ src/main/main.ts
@@ -5,38 +5,45 @@ import { format as formatURL } from "url";
 let win: BrowserWindow | null;
 
 const createWindow = () => {
-    win = new BrowserWindow({ height: 720, width: 1280 });
-    if (process.env.NODE_ENV !== "production") {
-        win.loadURL("http://localhost:8080");
+  win = new BrowserWindow({
+    height: 720,
+    width: 1280,
+  });
+  if (process.env.NODE_ENV !== "production") {
+    win.loadURL("http://localhost:8080");
 
-        // All dev module imports go here - webpack will filter them out for production
-        const { client } = require("electron-connect");
-        const { default: installExtension, VUEJS_DEVTOOLS } = require("electron-devtools-installer");
-        const devtron = require("devtron");
-        client.create(win);
-        installExtension(VUEJS_DEVTOOLS);
-        devtron.install();
-
-    } else {
-        win.loadURL(formatURL({
-            pathname: joinPaths(__dirname, "index.html"),
-            protocol: "file",
-            slashes: true,
-        }));
-    }
-    win.on("closed", () => win = null);
+    // All dev module imports go here - webpack will filter them out for production
+    const { client } = require("electron-connect");
+    const {
+      default: installExtension,
+      VUEJS_DEVTOOLS,
+    } = require("electron-devtools-installer");
+    // const devtron = require("devtron");
+    client.create(win);
+    installExtension(VUEJS_DEVTOOLS);
+    // devtron.install();
+  } else {
+    win.loadURL(
+      formatURL({
+        pathname: joinPaths(__dirname, "index.html"),
+        protocol: "file",
+        slashes: true,
+      })
+    );
+  }
+  win.on("closed", () => (win = null));
 };
 
 app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
 app.on("activate", () => {
-    if (win === null) {
-        createWindow();
-    }
+  if (win === null) {
+    createWindow();
+  }
 });
